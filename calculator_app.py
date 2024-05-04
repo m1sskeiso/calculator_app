4abc4339aae9ccb20102cded23341a994bcc63f1
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-05-04 19:21:15 +0700

    Operations

--- calculator_app.py
+++ calculator_app.py
@@ -1,4 +1,17 @@
 # Function for Addition
+def add(num1, num2):
+    return num1 + num2
+
 # Function for Subtraction
+def subtract(num1, num2):
+    return num1 - num2
+
 # Function for Multiplication
+def multiply(num1, num2):
+    return num1 * num2
+
 # Function for Division
+def divide(num1, num2):
+    if num2 == 0:
+        raise ValueError("Cannot divide by zero!")
+    return num1 / num2
